fe595e3b4fc182b7265c3684cf55633db60febdf
mezoo adel <mo3tz3adel@gmail.com>
2022-12-24 21:48:44 +0200

    trying to upload on domain

---
+++ jscode.js
@@ -0,0 +1,35 @@
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+window.addEventListener("load", function () {
+      $("#exampleModal").modal('show');
+    (function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })(); /**validation, ()required**/
+    // start dialoge on load
+    let dismissDialoge = document.querySelector("div[class=modal-footer] button:nth-child(2)");
+    dismissDialoge.onclick = function () {
+        document.querySelector("#validationCustom01").value = document.querySelector("#name-text").value;
+        localStorage.setItem("username", document.querySelector("#username-text").value);
+        document.querySelector("#validationCustomUsername").value = localStorage.getItem("username");
+         $("#exampleModal").modal('hide');
+
+    }
+    // <!--Navbar Spy-->
+    var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: 'nav[class=navbar]'
+    });
+})//end loading

86c08eeb5175d3d6a4fe7431e5f396f2495fcbe9
mezoo adel <mo3tz3adel@gmail.com>
2022-12-25 13:03:59 +0200

    modified Navbar

--- jscode.js
+++ jscode.js
@@ -1,7 +1,7 @@
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 window.addEventListener("load", function () {
-    // start dialoge on load
+    // start dialoge after loading
     //   $("#exampleModal").modal('show');
     (function () {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -19,17 +19,18 @@ window.addEventListener("load", function () {
                     form.classList.add('was-validated')
                 }, false)
             })
-    })(); /**validation, ()required**/
-    // fill form with dialoge inputs
-    let dismissDialoge = document.querySelector("div[class=modal-footer] button:nth-child(2)");
-    dismissDialoge.onclick = function () {
-        document.querySelector("#validationCustom01").value = document.querySelector("#name-text").value;
-        localStorage.setItem("username", document.querySelector("#username-text").value);
-        document.querySelector("#validationCustomUsername").value = localStorage.getItem("username");
-         $("#exampleModal").modal('hide');
+    })(); /**validation, **()required**/
+    // // fill form with dialoge inputs
+    // let dismissDialoge = document.querySelector("div[class=modal-footer] button:nth-child(2)");
+    // dismissDialoge.onclick = function () {
+    //     document.querySelector("#validationCustom01").value = document.querySelector("#name-text").value;
+    //     localStorage.setItem("username", document.querySelector("#username-text").value);
+    //     document.querySelector("#validationCustomUsername").value = localStorage.getItem("username");
+    //      $("#exampleModal").modal('hide');
 
-    }
-    // <!--Navbar Spy-->
+    // }
+
+    // <!--Navbar Spy-  -JQuery important for nav ->
     var scrollSpy = new bootstrap.ScrollSpy(document.body, {
         target: 'nav[class=navbar]'
     });
